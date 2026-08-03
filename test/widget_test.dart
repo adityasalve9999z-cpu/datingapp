415cb0da3e2c02ed1618b7ef435eaf5c7403062d
Aditya Salve <adityasalve9999z@gmail.com>
2026-08-04 00:06:49 +0530

    adding remaining screens

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:datingapp/main.dart';
+import 'package:datingapp/home.dart';
 import 'package:datingapp/screens/basic_to_advanced_screen.dart';
 
 void main() {
@@ -12,5 +13,11 @@ void main() {
   testWidgets('Basic to Advanced screen renders its title', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: BasicToAdvancedScreen()));
     expect(find.text('Basic to Advanced'), findsOneWidget);
+  });
+
+  testWidgets('Home screen shows the discovery shell', (tester) async {
+    await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
+    expect(find.text('Discover'), findsOneWidget);
+    expect(find.text('Matches'), findsOneWidget);
   });
 }
