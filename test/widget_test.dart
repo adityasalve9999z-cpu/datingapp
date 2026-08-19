fb28feee6853a33a5df3b2b4020e503d4e9f8523
Aditya Salve <adityasalve9999z@gmail.com>
2026-08-20 01:23:28 +0530

    chore: update plugin dependencies and add initial widget test file

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:provider/provider.dart';
 import 'package:datingapp/main.dart';
 import 'package:datingapp/home.dart';
 import 'package:datingapp/screens/basic_to_advanced_screen.dart';
+import 'package:datingapp/providers/auth_provider.dart';
+import 'package:datingapp/providers/profile_provider.dart';
+import 'package:datingapp/providers/discovery_feed_provider.dart';
+import 'package:datingapp/providers/matches_and_chat_provider.dart';
+import 'package:datingapp/providers/app_settings_provider.dart';
 
 void main() {
   testWidgets('App loads GlowDateApp successfully', (tester) async {
-    await tester.pumpWidget(const GlowDateApp());
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => AuthProvider()),
+          ChangeNotifierProvider(create: (_) => ProfileProvider()),
+          ChangeNotifierProvider(create: (_) => DiscoveryFeedProvider()),
+          ChangeNotifierProvider(create: (_) => MatchesAndChatProvider()),
+          ChangeNotifierProvider(create: (_) => AppSettingsProvider()),
+        ],
+        child: const GlowDateApp(),
+      ),
+    );
+    await tester.pump(const Duration(seconds: 3));
     expect(find.byType(GlowDateApp), findsOneWidget);
   });
 
@@ -17,7 +35,8 @@ void main() {
 
   testWidgets('Home screen shows the discovery shell', (tester) async {
     await tester.pumpWidget(const MaterialApp(home: HomeScreen()));
-    expect(find.text('Discover'), findsOneWidget);
-    expect(find.text('Matches'), findsOneWidget);
+    expect(find.text('Discover'), findsWidgets);
+    expect(find.text('Likes'), findsOneWidget);
   });
 }
+
